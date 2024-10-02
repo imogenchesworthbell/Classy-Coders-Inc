d9140f6ddcef6f4e52ef7861b793daf3b5b5c725
imogenchesworthbell <imogen.chesworthbell@barclays.com>
2024-10-02 12:34:45 +0100

    1st go

--- classes/Employee.js
+++ classes/Employee.js
@@ -1,8 +1,28 @@
 class Employee {
-
-
-
-    
+    #salary
+    #isHired
+    constructor(name, position, salary, isHired){
+        this.#isHired = isHired
+        this.#salary = salary
+        this.name = name
+        this.position = position
+    }
+    getSalary(){
+        return this.#salary
+    }
+    setSalary(amount){
+        this.#salary = amount
+    }
+    getStatus(){
+        return this.#isHired
+    }
+    setStatus(command){
+        if(command.toLowerCase() === 'hire'){
+             this.#isHired = true
+        }else if(command.toLowerCase() === 'fire'){
+            this.#isHired = false
+        }   
+    }
 }
 
 module.exports = {
